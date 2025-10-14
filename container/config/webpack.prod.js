7e6101990c22680b8ae6fb31b81b078dfa9214c9
Varshith K <varshithknaik@gmail.com>
2025-10-14 20:16:08 +0530

    feat(container): update webpack configuration for production and development environments

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,17 +1,26 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
-      }
-    ]
-  }
-}
+const commonConfig = require("./webpack.common");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
+const { merge } = require("webpack-merge");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/maketing/remoteEntry.js`,
+        // auth: `auth@${domain}/auth/remoteEntry.js`,
+        // dashboard: `dashboard@${domain}/dashboard/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
