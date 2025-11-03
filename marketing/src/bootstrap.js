bfefc3816807cb476231f3fa5e434cabb1d69d86
Varshith K <varshithknaik@gmail.com>
2025-11-03 19:42:57 +0530

    feat(MarketingApp): enhance navigation handling by adding initialPath support and defaultHistory option

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,27 +1,39 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
 import App from "./App";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
 // Mount function to start up the app
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
-  console.log(history);
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
   if (onNavigate) {
     history.listen((update) => {
-      console.log(update);
       onNavigate({ pathname: update.location.pathname });
     });
   }
   const root = createRoot(el);
   root.render(<App history={history} />);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      console.log(nextPathname, "parent navigate");
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are development and in isolation, call mount immediately
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
